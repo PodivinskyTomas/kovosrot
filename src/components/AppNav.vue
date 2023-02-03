ddafcb6e0e36e5d6f95ca03c69ea456da798e5b5
tomas.podivinsky <tomas.podivinsky@olc.cz>
2023-02-03 23:02:30 +0100

    Hamburger icon menu WIP

--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -3,7 +3,11 @@
         <a href="/" class="nav__title">
             <h3>Výkup kovového opadu Litovel</h3>
         </a>
-        <div>
+        <input id="menu-toggle" type="checkbox" />
+        <label class='menu-button-container' for="menu-toggle">
+            <div class='menu-button'></div>
+        </label>
+        <div class="menu">
             <a href="/#contact" class="app-nav__link">Kontakt</a>
             <a href="/#where-to-find-us" class="app-nav__link">Kde nás najít?</a>
             <a href="/#opening-hours" class="app-nav__link">Provozní doba</a>
@@ -25,6 +29,7 @@ nav {
     display: flex;
     align-items: center;
     justify-content: space-between;
+    flex-direction: row;
     background-color: var(--n8);
     padding-left: var(--content-padding);
     padding-right: var(--content-padding);
@@ -48,5 +53,110 @@ nav {
 .app-nav__link:hover {
     background-color: var(--p1);
     color: var(--white);
+}
+
+.menu>a {
+    overflow: hidden;
+}
+
+.menu-button-container {
+    display: none;
+    height: 100%;
+    width: 30px;
+    cursor: pointer;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+
+#menu-toggle {
+    display: none;
+}
+
+.menu-button,
+.menu-button::before,
+.menu-button::after {
+    display: block;
+    background-color: black;
+    position: absolute;
+    height: 4px;
+    width: 30px;
+    transition: transform 400ms cubic-bezier(0.23, 1, 0.32, 1);
+    border-radius: 2px;
+}
+
+.menu-button::before {
+    content: '';
+    margin-top: -8px;
+}
+
+.menu-button::after {
+    content: '';
+    margin-top: 8px;
+}
+
+#menu-toggle:checked+.menu-button-container .menu-button::before {
+    margin-top: 0px;
+    transform: rotate(405deg);
+}
+
+#menu-toggle:checked+.menu-button-container .menu-button {
+    background: rgba(255, 255, 255, 0);
+}
+
+#menu-toggle:checked+.menu-button-container .menu-button::after {
+    margin-top: 0px;
+    transform: rotate(-405deg);
+}
+
+@media (max-width: 700px) {
+    nav {
+        padding: 1rem;
+    }
+
+    .menu-button-container {
+        display: flex;
+    }
+
+    .menu {
+        position: absolute;
+        top: 0;
+        margin-top: calc(1.5625em + 2rem);
+        left: 0;
+        flex-direction: column;
+        width: 100%;
+        justify-content: center;
+        align-items: center;
+    }
+
+    #menu-toggle~.menu a {
+        height: 0;
+        margin: 0;
+        padding: 0;
+        border: 0;
+        transition: height 400ms cubic-bezier(0.23, 1, 0.32, 1);
+    }
+
+    #menu-toggle:checked~.menu a {
+        border: 1px solid #333;
+        height: 2.5em;
+        line-height: 2.5em;
+        padding: 0.5em;
+        transition: height 400ms cubic-bezier(0.23, 1, 0.32, 1);
+    }
+
+    .menu>a {
+        display: flex;
+        justify-content: center;
+        margin: 0;
+        padding: 0.5em 0;
+        width: 100%;
+        color: white;
+        background-color: #222;
+    }
+
+    .menu>a:not(:last-child) {
+        border-bottom: 1px solid #444;
+    }
 }
 </style>
