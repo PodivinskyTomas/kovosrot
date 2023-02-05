a4b0a92802522fd6b2b6be9d3612b09605b0c77d
tomas.podivinsky <tomas.podivinsky@olc.cz>
2023-02-05 17:29:46 +0100

    Czech nav anchors

--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -8,11 +8,10 @@
             <div class='menu-button'></div>
         </label>
         <div class="menu">
-            <a href="/#contact" class="app-nav__link">Kontakt</a>
-            <a href="/#where-to-find-us" class="app-nav__link">Kde nás najít?</a>
-            <a href="/#opening-hours" class="app-nav__link">Provozní doba</a>
-            <a href="/#about-us" class="app-nav__link">O nás</a>
-            <a href="/#news" class="app-nav__link">Aktuality</a>
+            <a href="/#kontakt" class="app-nav__link">Kontakt</a>
+            <a href="/#otevíracíDoba" class="app-nav__link">Provozní doba</a>
+            <a href="/#oNás" class="app-nav__link">O nás</a>
+            <a href="/#aktuality" class="app-nav__link">Aktuality</a>
         </div>
     </nav>
 </template>
