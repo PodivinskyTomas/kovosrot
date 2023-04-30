6434b7ead2442aad98e8afc35a0dfe4f93673e01
tomas.podivinsky <tomas.podivinsky@olc.cz>
2023-04-30 14:40:00 +0200

    block admin page indexation

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,97 +9,97 @@ import sitemap from "@astrojs/sitemap";
 import prefetch from "@astrojs/prefetch";
 import purgecss from "astro-purgecss";
 const articleCollection = {
-  name: "article",
-  label: "Aktuality",
-  label_singular: "Aktualita",
-  folder: CMS_COLLECTION_LOCATIONS.ARTICLES,
-  create: true,
-  slug: "{{year}}-{{month}}-{{day}}-{{title}}",
-  description: "Aktuální události. Zobrazují se na stránce hned v první sekci. Pište sem třeba dovolené nebo nějaké velké změny v chodu firmy.",
-  fields: [{
-    label: "Titulek",
-    name: "title",
-    widget: "string"
-  }, {
-    label: "Zpráva",
-    name: "body",
-    widget: "markdown",
-    sanitize_preview: true
-  }, {
-    label: "Jedná se o změnu pracovní doby?",
-    name: "is_opening_hours_change",
-    widget: "boolean",
-    required: false,
-    default: false
-  }, {
-    label: "Zobrazovat do (vyplňovat pouze pokud se jedná o dovolenou)",
-    name: "show_until",
-    hint: "Do kdy by se mělo zobrazovat upozornění na dovolenou",
-    default: "",
-    date_format: "DD.MM.YYYY",
-    time_format: false,
-    widget: "datetime",
-    required: false,
-    picker_utc: true
-  }, {
-    label: "Datum vytvoření",
-    name: "creation_date",
-    default: new Date(),
-    widget: "hidden"
-  }, {
-    label: "Layout",
-    name: "layout",
-    widget: "hidden",
-    default: "../../layouts/Article.astro"
-  }]
+    name: "article",
+    label: "Aktuality",
+    label_singular: "Aktualita",
+    folder: CMS_COLLECTION_LOCATIONS.ARTICLES,
+    create: true,
+    slug: "{{year}}-{{month}}-{{day}}-{{title}}",
+    description: "Aktuální události. Zobrazují se na stránce hned v první sekci. Pište sem třeba dovolené nebo nějaké velké změny v chodu firmy.",
+    fields: [{
+        label: "Titulek",
+        name: "title",
+        widget: "string"
+    }, {
+        label: "Zpráva",
+        name: "body",
+        widget: "markdown",
+        sanitize_preview: true
+    }, {
+        label: "Jedná se o změnu pracovní doby?",
+        name: "is_opening_hours_change",
+        widget: "boolean",
+        required: false,
+        default: false
+    }, {
+        label: "Zobrazovat do (vyplňovat pouze pokud se jedná o dovolenou)",
+        name: "show_until",
+        hint: "Do kdy by se mělo zobrazovat upozornění na dovolenou",
+        default: "",
+        date_format: "DD.MM.YYYY",
+        time_format: false,
+        widget: "datetime",
+        required: false,
+        picker_utc: true
+    }, {
+        label: "Datum vytvoření",
+        name: "creation_date",
+        default: new Date(),
+        widget: "hidden"
+    }, {
+        label: "Layout",
+        name: "layout",
+        widget: "hidden",
+        default: "../../layouts/Article.astro"
+    }]
 };
 function pageTextsItem(filename) {
-  return {
-    create: false,
-    delete: false,
-    file: CMS_COLLECTION_LOCATIONS.PAGE_TEXTS + "/" + filename
-  };
+    return {
+        create: false,
+        delete: false,
+        file: CMS_COLLECTION_LOCATIONS.PAGE_TEXTS + "/" + filename
+    };
 }
 const openingHoursCollection = {
-  ...pageTextsItem("provozni_doba.md"),
-  name: "provozni_doba",
-  label: "Provozní doba",
-  slug: "Provozní doba",
-  fields: [{
-    label: "Text",
-    name: "body",
-    widget: "markdown",
-    sanitize_preview: true
-  }]
+    ...pageTextsItem("provozni_doba.md"),
+    name: "provozni_doba",
+    label: "Provozní doba",
+    slug: "Provozní doba",
+    fields: [{
+        label: "Text",
+        name: "body",
+        widget: "markdown",
+        sanitize_preview: true
+    }]
 };
 const aboutUsCollection = {
-  ...pageTextsItem("about_us.md"),
-  name: "about_us",
-  label: "O nás",
-  slug: "O nás",
-  fields: [{
-    label: "Text",
-    name: "body",
-    widget: "markdown",
-    sanitize_preview: true
-  }]
+    ...pageTextsItem("about_us.md"),
+    name: "about_us",
+    label: "O nás",
+    slug: "O nás",
+    fields: [{
+        label: "Text",
+        name: "body",
+        widget: "markdown",
+        sanitize_preview: true
+    }]
 };
 const whatWeBuy = {
-  ...pageTextsItem("what_we_buy.md"),
-  name: "what_we_buy",
-  label: "Co vykupujeme?",
-  slug: "Co vykupujeme?",
-  fields: [{
-    label: "Seznam vykupovaného zboží",
-    name: "list",
-    widget: "list"
-  }]
+    ...pageTextsItem("what_we_buy.md"),
+    name: "what_we_buy",
+    label: "Co vykupujeme?",
+    slug: "Co vykupujeme?",
+    fields: [{
+        label: "Seznam vykupovaného zboží",
+        name: "list",
+        widget: "list"
+    }]
 };
 const pageTexts = {
-  name: "page_texts",
-  label: "Texty na stránce",
-  description: "Nastavení textů v jednotlivých sekcích stránky",
-  files: [aboutUsCollection, openingHoursCollection, whatWeBuy]
+    name: "page_texts",
+    label: "Texty na stránce",
+    description: "Nastavení textů v jednotlivých sekcích stránky",
+    files: [aboutUsCollection, openingHoursCollection, whatWeBuy]
 };
 
 // https://astro.build/config
@@ -108,21 +108,21 @@ const pageTexts = {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.sbernalitovel.cz/",
-  integrations: [NetlifyCMS({
-    previewStyles: [],
-    adminPath: "/administrace",
-    disableIdentityWidgetInjection: true,
-    config: {
-      locale: "cs",
-      search: false,
-      backend: {
-        name: 'git-gateway',
-        branch: 'master'
-      },
-      collections: [articleCollection, pageTexts]
-    }
-  }), vue(), compress(), sitemap(), prefetch({
-    throttle: 3
-  }), purgecss()]
+    site: "https://www.sbernalitovel.cz/",
+    integrations: [NetlifyCMS({
+        previewStyles: [],
+        adminPath: "/administrace",
+        disableIdentityWidgetInjection: true,
+        config: {
+            locale: "cs",
+            search: false,
+            backend: {
+                name: 'git-gateway',
+                branch: 'master'
+            },
+            collections: [articleCollection, pageTexts]
+        }
+    }), vue(), compress(), prefetch({
+        throttle: 3
+    }), purgecss()]
 });
