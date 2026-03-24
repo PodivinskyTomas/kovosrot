739d55b371ee8f76cef75017e07a71bd826f9732
tomas.podivinsky <tomas.podivinsky@olc.cz>
2026-03-24 10:36:53 +0100

    bump

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,15 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 import sitemap from "@astrojs/sitemap";
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.sbernalitovel.cz/",
+
   integrations: [sitemap({
     filter: page => page !== 'https://www.sbernalitovel.cz/administrace/' && page !== 'https://www.sbernalitovel.cz/administrace'
-  })]
+  })],
+
+  adapter: netlify()
 });
